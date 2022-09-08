ea35068738ec7305c8553dd412bdada612d009f9
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-09 00:55:18 +0900

    create middleware function

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export const config = {
+  matcher: '/',
+}
+
+export const middleware = (req: NextRequest): NextResponse => {
+  const COOKIE_NAME = 'ab-test'
+  let bucket = req.cookies.get(COOKIE_NAME)
+  if (!bucket) {
+    bucket = Math.random() < 0.5 ? 'new' : 'old'
+  }
+  const path = bucket === 'new' ? '/renewal' : '/'
+  const res = NextResponse.rewrite(new URL(path, req.url))
+
+  if (!req.cookies.get(COOKIE_NAME)) {
+    res.cookies.set(COOKIE_NAME, bucket)
+  }
+
+  return res
+}

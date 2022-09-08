73a164f2423f15693087fee23a48b3f9ba07c01c
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-09 00:41:26 +0900

    add style for icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center font-mono text-gray-800">
       <p className="my-3 text-xl">Concept A</p>
-      <CubeTransparentIcon />
+      <CubeTransparentIcon className="h-12 w-12 text-blue-500" />
     </div>
   )
 }

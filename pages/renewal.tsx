1099385018e51de4af53a8fcd7e720cdd09d43d6
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-09 00:41:38 +0900

    create Renewal component

---
+++ pages/renewal.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import { CubeTransparentIcon } from '@heroicons/react/24/outline'
+
+const Renewal: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center font-mono text-gray-800">
+      <p className="my-3 text-xl">Concept B</p>
+      <CubeTransparentIcon className="text-pink-500" />
+    </div>
+  )
+}
+
+export default Renewal
